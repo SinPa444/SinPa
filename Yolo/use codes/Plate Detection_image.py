b120cf107582bd21658de7452179d73411e68db9
SinPa <sinasheikhi96@gmail.com>
2025-04-06 10:47:15 +0330

    Update Plate Detection_image.py

--- Yolo/use codes/Plate Detection_image.py
+++ Yolo/use codes/Plate Detection_image.py
@@ -2,8 +2,8 @@ from ultralytics import YOLO
 import cv2 as cv
 
 
-model = YOLO("S:/AI/projects/car & plate detector5/weights/best.pt")
-img = cv.imread("S:/AI/Datasets/New folder/IMG_20250218_154311.jpg") # path the image
+model = YOLO("") # when training finished path your best.pt
+img = cv.imread("") # path the image
 results = model.predict(source=img, save=False)
 
 for r in results:
@@ -12,7 +12,7 @@ for r in results:
     r.boxes = filtered_boxes
 
 annotated_image = results[0].plot()
-annotated_image_resized = cv.resize(annotated_image, (640, 640))
+annotated_image_resized = cv.resize(annotated_image, (1920, 1080)) # you can change the Resolution
 cv.imshow("Plate Detection", annotated_image_resized)
 
 cv.waitKey(0)
